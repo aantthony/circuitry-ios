a0ce24b28c6863f32223ff875862234d9c461e4d
Anthony Foster <git@af.id.au>
2015-01-15 21:28:58 +1100

    Remove unused code

--- Circuitry/Circuitry/BoringInfoViewController.m
+++ Circuitry/Circuitry/BoringInfoViewController.m
@@ -17,33 +17,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     NSData *data = [NSData dataWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"Terms" withExtension:@"rtf"]];
     
-    NSAttributedString *attrString = [[NSAttributedString alloc] initWithData:data options:nil documentAttributes:nil error:nil];
-    
-    _textView.attributedText = attrString;
+    _textView.attributedText = [[NSAttributedString alloc] initWithData:data options:nil documentAttributes:nil error:nil];
 }
 
 - (IBAction)done:(id)sender {
-    [self dismissViewControllerAnimated:YES completion:^{
-        
-    }];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Circuitry/Circuitry/TutorialPage3ViewController.m
+++ Circuitry/Circuitry/TutorialPage3ViewController.m
@@ -17,26 +17,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    _descriptionText.layer.shadowColor = [[UIColor blackColor] CGColor];
-    _descriptionText.layer.shadowOffset = CGSizeMake(0.0, 1.0);
-    _descriptionText.layer.shadowRadius = 2.0;
+    _descriptionText.layer.shadowColor   = [[UIColor blackColor] CGColor];
+    _descriptionText.layer.shadowOffset  = CGSizeMake(0.0, 1.0);
+    _descriptionText.layer.shadowRadius  = 2.0;
     _descriptionText.layer.shadowOpacity = 1.0;
     _descriptionText.layer.masksToBounds = NO;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
